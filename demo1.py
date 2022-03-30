59e38df4505d9805a6912e9fa982744787dc2ee6
tanbin-hasnat-shehab <85411927+tanbin-hasnat-shehab@users.noreply.github.com>
2022-03-30 23:01:45 +0600

    Update demo1.py

--- demo1.py
+++ demo1.py
@@ -1,15 +1,13 @@
 from G_sheet import Google_sheet
 #make a public google sheet editor with anyone
-url='https://docs.google.com/spreadsheets/d/10pA-PS4iclTpgX0bx3RLWUhNMRapDeGQms5KQQKVe1c/edit#gid=0'
+url='https://docs.google.com/spreadsheets/d/1N2yFPBUSNqcBqmQND9ejt2tRdOwEr0DEGPbzHDx9w1M/edit#gid=0'
 
 
-x=Google_sheet()
+x=Google_sheet(url=url,sheet_name='ss')
 #inputting
-x.input_data(url=url,sheet_name='ss',position=[1,3],data=709999)
+x.input_data(position=[1,1],data='12121231443')
 
-'''
-#fetching data in a list of lists
-xx=x.show_data(url=url,sheet_name='ss')
-print(xx[3][2])
-print(xx)
-'''
+
+#fetching data from a cell
+print(x.show_data(pos=[1,1]))
+
